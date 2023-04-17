19996d768c6d95916b4a50e3962ddbbf26290b17
Edvinest <66338497+Edvinest@users.noreply.github.com>
2023-04-17 14:04:46 +0300

    basic ui + bad functionality

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+//TODO: Complete remaining tasks
+
 function playRound(playerSelection, computerSelection) {
     if (computerSelection == playerSelection) return "Tie!"
     else if (computerSelection == "paper" && playerSelection == "rock" ||
         computerSelection == "scissors" && playerSelection == "paper" ||
-        computerSelection == "rock" && playerSelection == "scissors") return "Computer wins this round!"
-    else return "Player wins this round!"
+        computerSelection == "rock" && playerSelection == "scissors") result.textContent = "Computer wins this round!"
+    else result.textContent = "Player wins this round!"
 }
 
 function getComputerChoice() {
@@ -11,10 +13,21 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-window.onload = function game() {
-    for (let index = 0; index < 5; index++) {
-        const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase()
-        const computerSelection = getComputerChoice()
-        console.log(playRound(playerSelection, computerSelection))
-    }
-}
+const container = document.querySelector('#container')
+const result = document.querySelector('#result')
+
+const btnRock = document.createElement('button')
+const btnScissors = document.createElement('button')
+const btnPaper = document.createElement('button')
+
+btnRock.textContent = "Rock"
+btnPaper.textContent = "Paper"
+btnScissors.textContent = "Scissors"
+
+container.append(btnRock, btnPaper, btnScissors)
+
+const buttons = document.querySelectorAll('#container button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => playRound(button.textContent, getComputerChoice))
+})
