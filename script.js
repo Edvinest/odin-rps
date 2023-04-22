f2c73252a0a7b93d7601d3ab9f2d46f9a015a8d0
Edvinest <66338497+Edvinest@users.noreply.github.com>
2023-04-22 21:05:24 +0300

    finished game

--- script.js
+++ script.js
@@ -1,48 +1,3 @@
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection == playerSelection) return "Tie!";
-    else if (computerSelection == "paper" && playerSelection == "rock" ||
-        computerSelection == "scissors" && playerSelection == "paper" ||
-        computerSelection == "rock" && playerSelection == "scissors") {
-        result.textContent = "Computer wins this round!";
-        computerScore++;
-    }
-
-    else {
-        result.textContent = "Player wins this round!";
-        playerScore++;
-    }
-}
-
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function determineVictor(){
-    if(playerScore === 5){
-        finalWinner("Player")
-    }
-    else if (computerScore === 5){
-        finalWinner("Computer")
-    }
-}
-
-function finalWinner(winner){
-    result.textContent = `${winner} won 5 rounds!`;
-    buttons.forEach(button => {
-        button.setAttribute("disabled", "true")
-    })
-
-    setTimeout(resetGame, 5000)
-}
-
-function resetGame(){
-    buttons.forEach(button => button.removeAttribute("disabled"));
-    result.textContent = "";
-    playerScore = 0;
-    computerScore = 0;
-}
-
 let playerScore = 0;
 let computerScore = 0;
 
@@ -61,8 +16,59 @@ container.append(btnRock, btnPaper, btnScissors);
 
 const buttons = document.querySelectorAll('#container button');
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        playRound(button.textContent.toLowerCase(), getComputerChoice());
-        determineVictor()
-    })})
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection == playerSelection) {
+        result.textContent = "Tie!";
+    }
+    else if (computerSelection == "paper" && playerSelection == "rock" ||
+        computerSelection == "scissors" && playerSelection == "paper" ||
+        computerSelection == "rock" && playerSelection == "scissors") {
+        result.textContent = "Computer wins this round!";
+        computerScore++;
+    }
+    else {
+        result.textContent = "Player wins this round!";
+        playerScore++;
+    }
+
+    determineVictor();
+}
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function handleClick(playerSelection) {
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+function determineVictor() {
+    if (playerScore === 5) {
+        finalWinner("Player");
+    }
+    else if (computerScore === 5) {
+        finalWinner("Computer");
+    }
+}
+
+function finalWinner(winner) {
+    result.textContent = `${winner} won 5 rounds!`;
+    buttons.forEach(button => {
+        button.setAttribute("disabled", "true");
+    })
+
+    setTimeout(resetGame, 5000)
+}
+
+function resetGame() {
+    buttons.forEach(button => button.removeAttribute("disabled"));
+    result.textContent = "";
+    playerScore = 0;
+    computerScore = 0;
+}
+
+btnRock.addEventListener('click', () => handleClick('rock'));
+btnPaper.addEventListener('click', () => handleClick('paper'));
+btnScissors.addEventListener('click', () => handleClick('scissors'));
